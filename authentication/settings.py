fc602f3fa13e6493d705094af26b79248e9a6da5
L <108204458+Laja1233@users.noreply.github.com>
2023-05-25 10:45:41 +0100

    Update settings.py

--- authentication/settings.py
+++ authentication/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-y&dliy-r=cjc(!^yb3q%@oo7^yd@1r$l+k_^xltt_ccm09bl01
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://www.goldelitedeals.com", 'https://djangogold-production.up.railway.app', 'djangogold-production.up.railway.app', 'localhost']
+ALLOWED_HOSTS = ["https://www.goldelitedeals.com", 'www.goldelitedeals.com', 'goldelitedeals.com', 'https://djangogold-production.up.railway.app', 'djangogold-production.up.railway.app', 'localhost']
+
 CSRF_TRUSTED_ORIGINS = ["https://www.goldelitedeals.com"] 
 
 
